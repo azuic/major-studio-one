82e64ec07356a7e89aec54e668e5fbb94cfb585c
Zui Chen <zuic@umich.edu>
2019-12-03 14:15:22 -0500

    final sketch bg camera

--- Final/sketch/script.js
+++ Final/sketch/script.js
@@ -2,73 +2,73 @@ $(document).ready(function() {
 
 
 
-  var padOne = new Audio('https://d1tutlfztia4ba.cloudfront.net/sounds/1.m4a');
+  var padOne = new Audio('https://d1tutlfztia4ba.cloudfront.net/sounds/1.wav');
   $('.pad-1').mousedown(function() {
     padOne.load()
     padOne.play();
   });
 
-  var padTwo = new Audio('https://d1tutlfztia4ba.cloudfront.net/sounds/2.m4a');
+  var padTwo = new Audio('https://d1tutlfztia4ba.cloudfront.net/sounds/2.wav');
   $('.pad-2').mousedown(function() {
     padTwo.load();
     padTwo.play();
   });
 
-  var padThree = new Audio('https://d1tutlfztia4ba.cloudfront.net/sounds/3.m4a');
+  var padThree = new Audio('https://d1tutlfztia4ba.cloudfront.net/sounds/3.wav');
   $('.pad-3').mousedown(function() {
     padThree.load();
     padThree.play();
   });
 
-  var padFour = new Audio('https://d1tutlfztia4ba.cloudfront.net/sounds/4.m4a');
+  var padFour = new Audio('https://d1tutlfztia4ba.cloudfront.net/sounds/4.wav');
   $('.pad-4').mousedown(function() {
     padFour.load();
     padFour.play();
   });
 
-  var padFive = new Audio('https://d1tutlfztia4ba.cloudfront.net/sounds/4.m4a');
+  var padFive = new Audio('https://d1tutlfztia4ba.cloudfront.net/sounds/5.wav');
   $('.pad-5').mousedown(function() {
     padFive.load();
     padFive.play();
   });
 
-  var padSix = new Audio('https://d1tutlfztia4ba.cloudfront.net/sounds/1.m4a');
+  var padSix = new Audio('https://d1tutlfztia4ba.cloudfront.net/sounds/6.wav');
   $('.pad-6').mousedown(function() {
     padSix.load();
     padSix.play();
   });
 
-  var padSeven = new Audio('https://d1tutlfztia4ba.cloudfront.net/sounds/3.m4a');
+  var padSeven = new Audio('https://d1tutlfztia4ba.cloudfront.net/sounds/7.wav');
   $('.pad-7').mousedown(function() {
     padSeven.load();
     padSeven.play();
   });
 
-  var padEight = new Audio('https://d1tutlfztia4ba.cloudfront.net/sounds/2.m4a');
+  var padEight = new Audio('https://d1tutlfztia4ba.cloudfront.net/sounds/8.wav');
   $('.pad-8').mousedown(function() {
     padEight.load();
     padEight.play();
   });
 
-  var padNine = new Audio('https://d1tutlfztia4ba.cloudfront.net/sounds/2.m4a');
+  var padNine = new Audio('https://d1tutlfztia4ba.cloudfront.net/sounds/9.wav');
   $('.pad-9').mousedown(function() {
     padNine.load();
     padNine.play();
   });
 
-  var padTen = new Audio('https://d1tutlfztia4ba.cloudfront.net/sounds/3.m4a');
+  var padTen = new Audio('https://d1tutlfztia4ba.cloudfront.net/sounds/10.wav');
   $('.pad-10').mousedown(function() {
     padTen.load();
     padTen.play();
   });
 
-  var padEleven = new Audio('https://d1tutlfztia4ba.cloudfront.net/sounds/1.m4a');
+  var padEleven = new Audio('https://d1tutlfztia4ba.cloudfront.net/sounds/11.wav');
   $('.pad-11').mousedown(function() {
     padEleven.load();
     padEleven.play();
   });
 
-  var padTwelve = new Audio('https://d1tutlfztia4ba.cloudfront.net/sounds/4.m4a');
+  var padTwelve = new Audio('https://d1tutlfztia4ba.cloudfront.net/sounds/12.wav');
   $('.pad-12').mousedown(function() {
     padTwelve.load();
     padTwelve.play();
