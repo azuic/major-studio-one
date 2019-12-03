4e7ab0c5e624b865beaa1985a3fedabb5885dea2
Zui Chen <zuic@umich.edu>
2019-12-03 13:34:17 -0500

    final sketch bg camera

--- Final/sketch/script.js
+++ Final/sketch/script.js
@@ -1,4 +1,23 @@
 $(document).ready(function() {
+  var constraints = { video: { facingMode: "user" }, audio: false };
+  const cameraView = document.querySelector("#camera--view"),
+      cameraOutput = document.querySelector("#camera--output"),
+      cameraSensor = document.querySelector("#camera--sensor")
+
+  function cameraStart() {
+      navigator.mediaDevices
+          .getUserMedia(constraints)
+          .then(function(stream) {
+          track = stream.getTracks()[0];
+          cameraView.srcObject = stream;
+      })
+      .catch(function(error) {
+          console.error("Oops. Something is broken.", error);
+      });
+    }
+
+  window.addEventListener("load", cameraStart, true);
+
 
   var padOne = new Audio('https://d1tutlfztia4ba.cloudfront.net/sounds/1.m4a');
   $('.pad-1').mousedown(function() {
